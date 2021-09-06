de2fa6f7a23b1cd423365e216c3731db0e8b1abc
ivanl15 <89130388+ivanl15@users.noreply.github.com>
2021-09-06 16:12:20 +0800

    Create AttacherViews.js

---
+++ views/AttacherViews.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import PlayerViews from './PlayerViews';
+
+const exports = {...PlayerViews};
+
+exports.Wrapper = class extends React.Component {
+  render() {
+    const {content} = this.props;
+    return (
+      <div className="Attacher">
+        <h2>Attacher (Bob)</h2>
+        {content}
+      </div>
+    );
+  }
+}
+
+exports.Attach = class extends React.Component {
+  render() {
+    const {parent} = this.props;
+    const {ctcInfoStr} = this.state || {};
+    return (
+      <div>
+        Please paste the contract info to attach to:
+        <br />
+        <textarea spellcheck="false"
+          className='ContractInfo'
+          onChange={(e) => this.setState({ctcInfoStr: e.currentTarget.value})}
+          placeholder='{}'
+        />
+        <br />
+        <button
+          disabled={!ctcInfoStr}
+          onClick={() => parent.attach(ctcInfoStr)}
+        >Attach</button>
+      </div>
+    );
+  }
+}
+
+exports.Attaching = class extends React.Component {
+  render() {
+    return (
+      <div>
+        Attaching, please wait...
+      </div>
+    );
+  }
+}
+
+exports.getPass = class extends React.Component {
+  render() {
+    const {parent} = this.props;
+    const {pass} = this.state || {};
+    return (
+      <div>
+        Password：<input
+          type='number'
+          onChange={(e) => this.setState({pass: e.currentTarget.value})}
+        /> <button 
+        disabled={!pass}
+        onClick={() => parent.setPass(this.state.pass)}>确认</button>
+        <br />
+      </div>
+    );
+  }
+}
+
+
+export default exports;
